06cc264742e0792af4ca88b9179346e662bfe142
Michael murray <michael.murray@hotmail.co.nz>
2015-11-09 18:23:45 +1300

    updated script and readme file
    updated script and readme file

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Vagrant-LAMP-Stack
+
+--Prerequisites--
+Virtual Box -- https://www.virtualbox.org/wiki/Downloads
+Vagrant -- https://www.vagrantup.com/
+
+An understanding of Ubuntu 12/Linux
+
+
+--Description—
+This vagrant script will install the following on Ubuntu 12.04
+
+* Apache2
+* MYSQL
+* PHP
+
+*PHP errors have been enabled
+*Apache mod rewrite has been enabled
+*passwords in this script have been set to ‘password’ I suggest you change this before running the script.
+
+Webmin has been commented out as it is not working as expected.
+
+
+--Instructions for running this script--
+
+NOTE: Anything in “” is a command
+1. Create a folder for your VM
+2. Run the following command in the folder you have created: “vagrant init hashicorp/precise32”
+You should see the following result:
+A `Vagrantfile` has been placed in this directory. You are now
+ready to `vagrant up` your first virtual environment! Please read
+the comments in the Vagrantfile as well as documentation on
+`vagrantup.com` for more information on using Vagrant.
+Clone this repository and place the VAGRANT and bootstrap file in the folder you have created above and overwrite the default VAGRANT file.
+Run the command “vagrant up”
+Once the script has completed run “vagrant ssh” this should show you the below output or similar 
+Host: 127.0.0.1
+Port: 22
+Username: vagrant 
+Use the above information to SHH with whatever SSH client you prefer. The username is vagrant and the password is vagrant.
+
+--TODO--
+* Get webmin install working
+* Add phpmyadmin install
+* Enable and configure SAMBA, also write a batch file to map the drive
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -10,11 +10,11 @@ sudo apt-get -y install apache2
 
 # Installing MySQL
 
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password webkits'
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password webkits'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password Password'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password Password'
 sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
 
-# Installing PHP and it's dependencies
+# Installing PHP
 sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
 
 
@@ -27,25 +27,11 @@ a2enmod rewrite > /dev/null 2>&1
 #allow override all
 sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
 
-#change the doc root
-#rm -rf /var/www
-#ln -fs /vagrant/public /var/www
-
-# install phpmyadmin
-
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/dbconfig-install boolean true'
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/app-password-confirm password webkits'
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/admin-pass password webkits'
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password webkits'
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2'?
-sudo apt-get -q -y install phpmyadmin
-
-
 #Enable PHP errors
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 
 #Install webmin
-sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
-wget http://prdownloads.sourceforge.net/webadmin/webmin_1.770_all.deb
-sudo dpkg -i webmin_1.770_all.deb
+#sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+#wget http://prdownloads.sourceforge.net/webadmin/webmin_1.770_all.deb
+#sudo dpkg -i webmin_1.770_all.deb
